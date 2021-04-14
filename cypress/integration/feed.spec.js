0b345a81f5297c9967bd89c4f430bffd541f26e3
Benoit Averty <benoit.averty@pm.me>
2021-04-14 10:04:06 +0200

    test(feed): add a test for the revalidation feature

--- cypress/integration/feed.spec.js
+++ cypress/integration/feed.spec.js
@@ -34,4 +34,55 @@ describe("feed", () => {
     cy.scrollTo(0, 1000);
     cy.contains("You've read everything !");
   });
+
+  it("revalidates on focus", () => {
+    cy.fixture("global-feed/two-articles.json").then(body => {
+      cy.request(
+        "POST",
+        `${Cypress.env("MOCK_SERVER")}/__admin/mappings?apiToken=${Cypress.env("MOCKLAB_TOKEN")}`,
+        {
+          request: {
+            method: "GET",
+            url: "/api/articles?offset=0&limit=20",
+          },
+          response: {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+          },
+        },
+      );
+    });
+
+    cy.visit("/");
+
+    cy.contains("An article about Remix");
+    cy.contains("An article about React");
+    cy.get('[data-testid="article-summary"]').should("have.length", 2);
+
+    cy.fixture("global-feed/three-articles.json").then(body => {
+      cy.request(
+        "POST",
+        `${Cypress.env("MOCK_SERVER")}/__admin/mappings?apiToken=${Cypress.env("MOCKLAB_TOKEN")}`,
+        {
+          request: {
+            method: "GET",
+            url: "/api/articles?offset=0&limit=20",
+          },
+          response: {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+          },
+        },
+      );
+    });
+
+    cy.document().trigger("focus");
+
+    cy.contains("An article about Cypress");
+    cy.contains("An article about Remix");
+    cy.contains("An article about React");
+    cy.get('[data-testid="article-summary"]').should("have.length", 3);
+  });
 });
